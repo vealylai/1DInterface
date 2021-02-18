f2a9dfa1520e9c24a2c5a582b826282e1cdfd2c8
Marcelo Coelho <marcelosco@gmail.com>
2021-02-18 10:24:01 -0500

    More detailed commenting

--- Interface1D/A_Joystick/A_joystick.ino
+++ Interface1D/A_Joystick/A_joystick.ino
@@ -1,6 +1,7 @@
 /*
 
-#include
+// DON'T USE THIS DOC YET. IT'S NOT READY.
+
 http://www.asciitable.com/
 
 */
@@ -11,20 +12,20 @@ http://www.asciitable.com/
 // Setup a RoraryEncoder for pins A2 and A3:
 RotaryEncoder encoder(A0, A1);
 
-
 #define OFF 0
-#define ON  1
+#define ON 1
 
 bool keyA = OFF;
 bool keyB = OFF;
 bool keyX = OFF;
 bool keyY = OFF;
 
-void setup() {
+void setup()
+{
 
   Serial.begin(57600);
   Serial.println("yo");
-  
+
   // make pin 2 an input and turn on the
   // pullup resistor so it goes high unless
   // connected to ground:
@@ -32,62 +33,68 @@ void setup() {
   Keyboard.begin();
 }
 
-void loop() {
+void loop()
+{
 
   static int pos = 0;
   encoder.tick();
 
   int newPos = encoder.getPosition();
-  if (pos != newPos) {
+  if (pos != newPos)
+  {
     Serial.print(newPos);
     Serial.println();
 
-    if (newPos > pos) {
-       Keyboard.write(69);
+    if (newPos > pos)
+    {
+      Keyboard.write(69);
     }
 
-    if (newPos < pos) {
-       Keyboard.write(70);
+    if (newPos < pos)
+    {
+      Keyboard.write(70);
     }
-    
+
     pos = newPos;
-  } 
+  }
 
-  if ((digitalRead(2) == HIGH) && keyA == OFF) {
+  if ((digitalRead(2) == HIGH) && keyA == OFF)
+  {
     keyA = ON;
     Keyboard.write(65);
   }
-  if (digitalRead(2) == LOW) {
+  if (digitalRead(2) == LOW)
+  {
     keyA = OFF;
   }
 
-
-  if ((digitalRead(3) == HIGH) && keyB == OFF) {
+  if ((digitalRead(3) == HIGH) && keyB == OFF)
+  {
     keyB = ON;
     Keyboard.write(68);
   }
-  if (digitalRead(3) == LOW) {
+  if (digitalRead(3) == LOW)
+  {
     keyB = OFF;
   }
 
-
-  if ((digitalRead(4) == HIGH) && keyX == OFF) {
+  if ((digitalRead(4) == HIGH) && keyX == OFF)
+  {
     keyX = ON;
     Keyboard.write(67);
   }
-  if (digitalRead(4) == LOW) {
+  if (digitalRead(4) == LOW)
+  {
     keyX = OFF;
   }
 
-
-  if ((digitalRead(5) == HIGH) && keyY == OFF) {
+  if ((digitalRead(5) == HIGH) && keyY == OFF)
+  {
     keyY = ON;
     Keyboard.write(68);
   }
-  if (digitalRead(5) == LOW) {
+  if (digitalRead(5) == LOW)
+  {
     keyY = OFF;
   }
-
-
-  
 }
--- Interface1D/P5_Interface1D/sketch.js
+++ Interface1D/P5_Interface1D/sketch.js
@@ -1,54 +1,61 @@
+/* /////////////////////////////////////
+
+  4.043 / 4.044 Design Studio: Advanced Interactions
+  February  18, 2021
+  Marcelo Coelho
+
+  If you come from Processing, there is a Processing version here. 
+  Keep in mind that there are some differences between both versions:
+  https://github.com/marcelocoelho/Interface1D
+
+*/ /////////////////////////////////////
 
 
-// Processing version is here:
-// https://github.com/marcelocoelho/Interface1D
+let displaySize = 30;   // how many pixels are visible in the game
+let pixelSize = 20;     // how big should they look on screen
 
-// Try to implement serial port like this: 
-// http://mactech.sheridanc.on.ca/team/mark-galaszkiewicz/using-p5serialserver/
-
-
-
-
-let displaySize = 30;
-let pixelSize = 20;
-
-let playerOne;
+let playerOne;    // Adding 2 players to the game
 let playerTwo;
+let target;       // and one target for players to catch.
 
-let target;
+let display;      // Aggregates our final visual output before showing it on the screen
 
-let display;
-let controller;
+let controller;   // This is where the state machine and software logic lives
 
-let collisionAnimation;
+let collisionAnimation;   // Where we store and manage the collision animation
 
-let score;
+let score;        // Where we keep track of score and winner
 
 
 
 function setup() {
 
-  createCanvas((displaySize*pixelSize), pixelSize);
+  createCanvas((displaySize*pixelSize), pixelSize);     // dynamically sets canvas size
 
-  display = new Display(displaySize, pixelSize);  // aggregates our final visual output
+  display = new Display(displaySize, pixelSize);        //Initializing the display
 
-  playerOne = new Player(color(255,0,0), parseInt(random(0,displaySize)), displaySize);  // Adding 2 players to the game
+  playerOne = new Player(color(255,0,0), parseInt(random(0,displaySize)), displaySize);   // Initializing players
   playerTwo = new Player(color(0,0,255), parseInt(random(0,displaySize)), displaySize);
 
-  target = new Player(color(255,255,0), parseInt(random(0,displaySize)), displaySize);              // and one target for players to catch.
+  target = new Player(color(255,255,0), parseInt(random(0,displaySize)), displaySize);    // Initializing target using the Player class 
 
-  collisionAnimation = new Animation();
+  collisionAnimation = new Animation();     // Initializing animation
 
-  controller = new Controller();
+  controller = new Controller();            // Initializing controller
 
-  score = {max:3, winner:color(0,0,0)};
+  score = {max:3, winner:color(0,0,0)};     // score stores max number of points, and color 
 
 }
 
 function draw() {
-  background(0, 0, 0);
 
+  // start with a blank screen
+  background(0, 0, 0);    
+
+  // Runs state machine at determined framerate
   controller.update();
+
+  // After we've updated our states, we show the current one 
   display.show();
 
 }
