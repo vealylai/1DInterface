a03582768a1e860cbb42156b545bdc4d183358a9
Marcelo Coelho <marcelosco@gmail.com>
2021-03-02 13:12:03 -0500

    add helpers and keyboard example

--- Interface1D/A_Joystick/A_joystick.ino
+++ Interface1D/A_Joystick/A_joystick.ino
@@ -1,20 +1,24 @@
 /*
 
-// DON'T USE THIS DOC YET. IT'S NOT READY.
+This is a simple example that allows you to connect 4 buttons and a rotary encoder to your Arduino.
+The Arduino acts as a keyboard by outputting button presses.
 
+You will need this table to figure the code for the characters you are trying to output.
 http://www.asciitable.com/
 
 */
 
-#include <Keyboard.h>
-#include <RotaryEncoder.h>
+#include <Keyboard.h>      // include library that let's Arduino act as a keyboard
+#include <RotaryEncoder.h> // include rotary encoder library
 
-// Setup a RoraryEncoder for pins A2 and A3:
+// Setup a RoraryEncoder for pins A0 and A1:
 RotaryEncoder encoder(A0, A1);
 
+// some useful values
 #define OFF 0
 #define ON 1
 
+// start by assuming no buttons are pressed
 bool keyA = OFF;
 bool keyB = OFF;
 bool keyX = OFF;
@@ -23,19 +27,26 @@ bool keyY = OFF;
 void setup()
 {
 
+  // connect to serial port for debugging
   Serial.begin(57600);
-  Serial.println("yo");
 
   // make pin 2 an input and turn on the
   // pullup resistor so it goes high unless
   // connected to ground:
   pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+
+  // start the keyboard
   Keyboard.begin();
 }
 
 void loop()
 {
 
+  // Read the encoder and output its value
+  /////////////////////////////////////////
   static int pos = 0;
   encoder.tick();
 
@@ -47,21 +58,24 @@ void loop()
 
     if (newPos > pos)
     {
-      Keyboard.write(69);
+      Keyboard.write(69); // E
     }
 
     if (newPos < pos)
     {
-      Keyboard.write(70);
+      Keyboard.write(70); // F
     }
 
     pos = newPos;
   }
 
+  // All the key presses happen here
+  //////////////////////////////////////////////
+
   if ((digitalRead(2) == HIGH) && keyA == OFF)
   {
     keyA = ON;
-    Keyboard.write(65);
+    Keyboard.write(65); // A
   }
   if (digitalRead(2) == LOW)
   {
@@ -71,7 +85,7 @@ void loop()
   if ((digitalRead(3) == HIGH) && keyB == OFF)
   {
     keyB = ON;
-    Keyboard.write(68);
+    Keyboard.write(68); // D
   }
   if (digitalRead(3) == LOW)
   {
@@ -81,7 +95,7 @@ void loop()
   if ((digitalRead(4) == HIGH) && keyX == OFF)
   {
     keyX = ON;
-    Keyboard.write(67);
+    Keyboard.write(74); // J
   }
   if (digitalRead(4) == LOW)
   {
@@ -91,7 +105,7 @@ void loop()
   if ((digitalRead(5) == HIGH) && keyY == OFF)
   {
     keyY = ON;
-    Keyboard.write(68);
+    Keyboard.write(76); // L
   }
   if (digitalRead(5) == LOW)
   {
